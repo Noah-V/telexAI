ebe7b673c0a6ec8832f090cf5eb22dd773dda4fe
Noah-V <noahvikoo3@gmail.com>
2025-02-21 21:36:16 +0100

    branch

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,11 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
+	console.log("Default config on startup:", defaultConfig);
 	return console.log(`Express is listening at http://localhost:${port}`);
 });
 
 app.get("/integration", (req, res) => {
+	console.log("Trying integration rn");
 	res.send(defaultConfig);
 });
